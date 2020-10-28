cd35232394eb5ade51008a9928442cc92865e085
NishantChauhan032 <nishantkr0210@gmail.com>
2020-10-28 16:50:46 +0530

    [NISHANT] add : ability to create employee_payroll table

--- PayrollService.sql
+++ PayrollService.sql
@@ -4,4 +4,15 @@ create database payroll_service;
 use payroll_service;
 Select database();
 
+# UC 2- To create employee payroll table
+create table employee_payroll                                     #Create table
+(
+ID       int unsigned NOT NULL auto_increment,                    #Set ID as auto increment
+Name     Varchar(150) NOT NULL,                                    
+Salary   double NOT NULL,
+START    DATE NOT NULL,
+primary key (ID)                                                  #Set ID as primary key
+);        
+describe employee_payroll;                                        #To describe table
+
 
